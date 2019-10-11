3f272a4b6d27b5e7fc66db67ee62e866bce60ea1
Sushmmita-Rudra <S537491@NWMISSOURI.EDU>
2019-10-11 13:19:35 -0500

    Added js and css file for sketch counter

---
+++ components/sketchCount/sketchCount.js
@@ -0,0 +1,19 @@
+const count = (x, y) => { 
+    return x * y 
+}
+
+const sketchCount = new Vue({
+  el: '#sketchCount',
+  data: {
+    yourName: 'Rudra',
+    stories: 5,
+    sketchesPerStory: 3
+  },
+  computed: {
+    sketchCounter: function () {
+      const i = parseInt(this.stories)
+      const j = parseInt(this.sketchesPerStory)
+      return `${this.yourName}, your sketches are ${count(i, j)}.`
+    }
+  }
+})
